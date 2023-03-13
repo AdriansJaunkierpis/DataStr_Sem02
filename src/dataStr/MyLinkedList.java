e37855e8152f53ae453b3ba4a70d4f927d53161d
AdriansJaunkierpis <adrians.jaunkierpis@gmail.com>
2023-03-13 10:39:47 +0200

    LinkedList add functions

--- src/dataStr/MyLinkedList.java
+++ src/dataStr/MyLinkedList.java
@@ -1,5 +1,59 @@
 package dataStr;
 
-public class MyLinkedList {
-
+public class MyLinkedList<T> {
+	private MyNode first = null;
+	private MyNode last = null;
+	private int elementCounter = 0;
+	
+	//TODO isFull() isEmpty() howManyElements()
+	
+	public void add(T newElement) {
+		//TODO verify not full
+		if (elementCounter == 0) {
+			MyNode<T> newNode = new MyNode<T>(newElement);
+			first = newNode;
+			last = newNode;
+			elementCounter++;
+		} else {
+			MyNode<T> newNode = new MyNode<T>(newElement);
+			last.setNext(newNode);
+			newNode.setPrev(last);
+			last = newNode;
+			elementCounter++;
+		}
+	}
+	
+	public void add(T newElement, int position) throws Exception {
+		//TODO verify not full
+		if (position < 0 || position > elementCounter) {
+			throw (new Exception("Wrong position"));
+		}
+		//add at the beginning
+		if (position == 0) {
+			MyNode<T> newNode = new MyNode<T>(newElement);
+			first.setNext(first);
+			first = newNode;
+			elementCounter++;
+		}
+		//add at the end
+		else if (position == elementCounter) {
+			add(newElement);
+		}
+		//add in the middle
+		else {
+			MyNode tempNode = first;
+			for (int i = 0; i < position - 1; i++) {
+				tempNode = tempNode.getNext();
+			}
+			MyNode tempNode2 = tempNode.getNext();
+			MyNode<T> newNode = new MyNode<T>(newElement);
+			
+			tempNode.setNext(newNode);
+			newNode.setPrev(tempNode);
+			
+			tempNode2.setPrev(newNode);
+			newNode.setNext(tempNode2);
+			elementCounter++;
+		}
+	}
 }
--- src/dataStr/MyNode.java
+++ src/dataStr/MyNode.java
@@ -1,5 +1,46 @@
 package dataStr;
 
-public class MyNode {
+public class MyNode<T> {
+	
+	private T element;
+	private MyNode next = null;
+	private MyNode prev = null;
 
+	
+	public T getElement() {
+		return element;
+	}
+
+	public void setElement(T inputElement) {
+		if (inputElement != null) {
+			element = inputElement;
+		} else {
+			element = (T) new Object();
+		}
+	}
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+
+	public MyNode getPrev() {
+		return prev;
+	}
+
+	public void setPrev(MyNode prev) {
+		this.prev = prev;
+	}
+
+
+	public MyNode(T inputElement) {
+		setElement(inputElement);
+	}
+	
+	public String toString() {
+		return "" + element;
+	}
 }
